0bfc2ed7cb5894fc8054ffc99ee952b288b87f60
Johan <johan_carlsen@hotmail.com>
2021-11-23 20:18:40 +0100

    commit

--- challenge_A.py
+++ challenge_A.py
@@ -68,7 +68,7 @@ initial_time, pos, vel = landing.orient()
 
 A_lander = mission.lander_area     # cross section area of the lander in m^2
 A_craft = mission.spacecraft_area # cross section area of the craft in m^2
-A_parachute = 8 # m^2
+A_parachute = 30 # m^2
 A_lander_parachute = A_lander + A_parachute
 R = system.radii[6] * 1000 # m
 m_planet = system.masses[6] * const.m_sun # kg
@@ -151,19 +151,21 @@ def trajectory_lander(initial_time, initial_velocity, initial_position, simulati
 
         g_acceleration = G * m_planet / r_norm**2
 
-        # F_d = F_drag(position[:,i], velocity[:,i], A_craft)
-        # F_G = -m_craft * g_acceleration * unit_r
-
-        if t[i] > 600:
-            if t[i] < 600 + dt:
+        if t[i] > 150:
+            if t[i] < 150 + dt:
                 velocity[:,i] *= 0.25
                 print('')
                 print('Lander launched with dv=', velocity[:,i] / 4, 'm/s at t=', t[i], 's.')
 
             F_G = -m_lander * g_acceleration * unit_r
-
-            if t[i] > 1800:
-                if t[i] < 1800 + dt:
+            '''
+            HERE THERE IS SOMETHING WRONG,
+            PLEASE TRY TO FIX!
+            IT SEEMS AS IF THE PLOT FOR v_r DOES NOT
+            INCLUDE PARACHUTE AT GIVEN TIME.
+            '''
+            if t[i] > 450:
+                if t[i] < 450 + dt:
                     print('')
                     print('Opened parachute at t=', t[i], 's.')
 
@@ -206,54 +208,75 @@ def trajectory_lander(initial_time, initial_velocity, initial_position, simulati
 quarter = 900 # 15 minutes = 900 s
 
 t1 = time()
-t, r, v, drag_preassure, F_drag = trajectory_lander(initial_time, 0.9*vel, pos, 12*quarter)
+t, r, v, drag_preassure, F_drag = trajectory_lander(initial_time, 0.8*vel, pos, 25*quarter)
 t2 = time()
 print('Simulation took', t2-t1, 's to complete.')
 # print(f'Height above ground after t={t[-1]} s: {np.linalg.norm(r[:,-1]) - R} m.')
 
+r_radial = np.sqrt(r[0,:]**2 + r[1,:]**2)
+r_theta = np.arctan(r[1,:] / r[0,:])
+
+vr = (r[0,:] * v[0,:] + r[1,:] * v[1,:]) / np.sqrt(r[0,:]**2 + r[1,:]**2)
+v_theta = r_radial * ((r[0,:] * v[1,:] - v[0,:] * r[1,:]) / (r[0,:]**2 + r[1,:]**2))
+
+print('')
+if abs(vr[-1]) < 3:
+    print(f'Soft landing performed, with final radial velocity v_r={vr[-1]} m/s.')
+
+else:
+    print(f'Hard landing performed, with final radial velocity v_r={vr[-1]} m/s. Hope you braced.')
+
 plt.figure()
 
-theta_planet = np.linspace(0, 2*np.pi, 1001)
-x_planet = R * np.cos(theta_planet)
-y_planet = R * np.sin(theta_planet)
-
-plt.title('Simulation of landing')
-plt.plot(r[0,:], r[1,:], 'r', label='Position')
-plt.plot(0,0, 'ko', label='Center')
-plt.plot(x_planet, y_planet, 'b', lw=0.5, label='Surface')
-plt.axis('equal')
+plt.title('Radial velocity')
+plt.plot(t, vr, label='v_r')
 plt.legend()
+plt.xlabel('Time [s]')
+plt.ylabel('Velocity [m/s]')
 
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-
-ax1.set_title('Drag preassure on lander')
-ax1.plot(t, drag_preassure, label='P')
-ax1.legend()
-ax1.set_ylabel('Preassure [Pa]')
-
-ax2.set_title('Force on parachute')
-ax2.plot(t, np.linalg.norm(F_drag, axis=0), label='Drag')
-ax2.legend()
-ax2.set_ylabel('Force [N]')
-ax2.set_xlabel('Time [s]')
-
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, figsize=(8, 6))
-
-ax1.set_title('Height above surface')
-ax1.plot(t, np.linalg.norm(r, axis=0) - R, label='h')
-ax1.legend()
-ax1.set_ylabel('Height [m]')
-
-ax2.set_title('Velocity')
-ax2.plot(t, v.T)
-ax2.legend(['vx', 'vy'])
-ax2.set_ylabel('Velocity [m/s]')
-
-ax3.set_title('Absolute velocity')
-ax3.plot(t, np.linalg.norm(v, axis=0), label='|v|')
-ax3.legend()
-ax3.set_ylabel('Velocity [m/s]')
-ax3.set_xlabel('Time [s]')
+# plt.figure()
+#
+# theta_planet = np.linspace(0, 2*np.pi, 1001)
+# x_planet = R * np.cos(theta_planet)
+# y_planet = R * np.sin(theta_planet)
+#
+# plt.title('Simulation of landing')
+# plt.plot(r[0,:], r[1,:], 'r', label='Position')
+# plt.plot(0,0, 'ko', label='Center')
+# plt.plot(x_planet, y_planet, 'b', lw=0.5, label='Surface')
+# plt.axis('equal')
+# plt.legend()
+#
+# fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+#
+# ax1.set_title('Drag preassure on lander')
+# ax1.plot(t, drag_preassure, label='P')
+# ax1.legend()
+# ax1.set_ylabel('Preassure [Pa]')
+#
+# ax2.set_title('Force on parachute')
+# ax2.plot(t, np.linalg.norm(F_drag, axis=0), label='Drag')
+# ax2.legend()
+# ax2.set_ylabel('Force [N]')
+# ax2.set_xlabel('Time [s]')
+#
+# fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, figsize=(8, 6))
+#
+# ax1.set_title('Height above surface')
+# ax1.plot(t, np.linalg.norm(r, axis=0) - R, label='h')
+# ax1.legend()
+# ax1.set_ylabel('Height [m]')
+#
+# ax2.set_title('Velocity')
+# ax2.plot(t, v.T)
+# ax2.legend(['vx', 'vy'])
+# ax2.set_ylabel('Velocity [m/s]')
+#
+# ax3.set_title('Absolute velocity')
+# ax3.plot(t, np.linalg.norm(v, axis=0), label='|v|')
+# ax3.legend()
+# ax3.set_ylabel('Velocity [m/s]')
+# ax3.set_xlabel('Time [s]')
 
 plt.show()
 
